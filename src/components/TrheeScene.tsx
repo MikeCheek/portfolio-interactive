564255628d1974823dda7209ff7cb7e4bb0c628b
Michele Pulvirenti <pulvirentimichele00@gmail.com>
2025-02-11 19:39:24 +0100

    Limited orbit controls

--- src/components/TrheeScene.tsx
+++ src/components/TrheeScene.tsx
@@ -13,12 +13,16 @@ const ThreeScene: React.FC = () => {
   // useFrame((state, delta) => {
   //   easing.damp3(state.camera.position, [-state.pointer.x * 2, state.pointer.y + 1.5, 10], 0.3, delta); // Move camera
   // });
+  const dev = !process.env.NODE_ENV || process.env.NODE_ENV === "development"
   return (
     <>
       <Environment preset="sunset" />
       <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
       <pointLight position={[10, 10, 10]} />
-      <OrbitControls />
+      <OrbitControls
+        maxPolarAngle={dev ? undefined : Math.PI / 2}  // Limit vertical rotation to not go upside down
+        minPolarAngle={dev ? undefined : Math.PI / 3}  // Limit minimum vertical rotation
+      />
       {/* <Carousel /> */}
 
       <group position={[0, -1, 0]}>
